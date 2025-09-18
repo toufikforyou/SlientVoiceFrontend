3846734f732e7bcc1439a0dd166c87fc07c8918a
Minhajul Islam <mmminhaj221@gmail.com>
2025-09-18 22:32:55 +0600

    firebase integrated

--- src/Context/Global_Provider.jsx
+++ src/Context/Global_Provider.jsx
@@ -1,14 +1,46 @@
-import { createContext } from "react";
+import { createContext, useState , useEffect } from "react";
 import { createUserWithEmailAndPassword, FacebookAuthProvider, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { app } from "../../firebase.config";
 
 export const GlobalContext = createContext(null);
-const Global_Provider = ({children}) => {
+const Global_Provider = ({ children }) => {
     const name = "Minhajul Islam";
-    
-    
+    const [globalLoading , setGlobalLoading] = useState(false);
+    const auth = getAuth(app);
+    const [user , setUser] = useState(null);
+    const google_provider = new GoogleAuthProvider();
+
+    const connect_google = () => {
+        setGlobalLoading(true);
+        return signInWithPopup(auth, google_provider);
+    }
+    const log_out = () => {
+        return signOut(auth);
+    }
+
+     useEffect(()=>{
+        // holds the user
+        const unsubscirbe = onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+            setGlobalLoading(false);
+            if(currentUser){
+                // issue a token
+                console.log(currentUser);
+            }else{
+                // remove the token
+                console.log("User logged out!")
+            }
+        });
+
+        return ()=>{
+            unsubscirbe();
+        }
+
+    },[])
+
 
     const globalData = {
-        name
+        name, connect_google, log_out , globalLoading
     }
     return (
         <GlobalContext.Provider value={globalData}>
--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,32 +1,42 @@
 import axios from "axios";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router";
+import { GlobalContext } from "../../../Context/Global_Provider";
 
 const Login = () => {
     const [form, setForm] = useState({ email: "", password: "" });
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
+    const {connect_google, log_out } = useContext(GlobalContext);
+
+
+    const handleGoogleLogin =()=>{
+        connect_google().then(res=>{
+            console.log(res)
+        })
+    } 
+    
     const handleChange = e =>
         setForm(prev => ({ ...prev, [e.target.name]: e.target.value }));
 
-    // const handleSubmit = async e => {
-        // e.preventDefault();
-        // setLoading(true);
-        // setError("");
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setLoading(true);
+        setError("");
         // try {
-            // const res = await axios.post("https://event-manager-server-eta.vercel.app/api/users/login", form);
-            // console.log(res.data.user);
-            // localStorage.setItem("user", JSON.stringify(res.data.user));
-            // navigate("/");
+        //     const res = await axios.post("https://event-manager-server-eta.vercel.app/api/users/login", form);
+        //     console.log(res.data.user);
+        //     localStorage.setItem("user", JSON.stringify(res.data.user));
+        //     navigate("/");
         // } catch (err) {
-            // console.log(err.data);
-            // setError(err.response?.data?.message || "Login failed");
+        //     console.log(err.data);
+        //     setError(err.response?.data?.message || "Login failed");
         // } finally {
-            // setLoading(false);
+        //     setLoading(false);
         // }
-    // };
+    };
 
     return (
         <div className="flex min-h-screen items-center justify-center">
@@ -69,6 +79,9 @@ const Login = () => {
                     </button>
                 </div>
                 {/* <hr className="mt-5 border-gray-400 " /> */}
+                <p onClick={()=>{handleGoogleLogin()}}>Google login</p>
+                <p onClick={()=>{log_out()}}> Log out</p>
+
                 <div className="text-center mt-5">
                     New Here ? <span><Link to="/signup" className="text-[#5f45ba] hover:underline">Create an account</Link></span>
                 </div>
